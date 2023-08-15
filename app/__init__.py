15816c3ec480e94bc345ad449e8246ec24dd9dbf
Maria Eduarda Nogueira <dudanog_@hotmail.com>
2023-08-15 11:55:55 -0300

    15/08/2023

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
-from flask import Flask 
-app = Flask (__name__)
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-from app import routes  
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+app.config.from_object(Config)
+from app import routes, models 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app 
-from flask import render_template
-
+from app import app, db
+from flask import render_template, url_for, request, flash
+from app.forms import Contato
+from app.models import ContatoModel
 @app.route('/')
 def index():
     return render_template('index.html',titulo = 'Página inicial')
@@ -15,16 +16,28 @@ def Projeto():
 
 #projeto para html
 
-@app.route('/contatos')
+@app.route('/contatos', methods=['POST', 'GET'])
 def contatos():
-    return render_template('contatos.html', titulo = 'Contatos')
+    dados_formulario = None
+    formulario = Contato()
+    print('Acessou a rota contatos!')
+    if formulario.validate_on_submit():
+        flash('Esse formulário foi enviado com sucesso!')
+        nome = formulario.nome.data
+        email = formulario.email.data
+        telefone = formulario.telefone.data
+        mensagem = formulario.mensagem.data
+        
+        novo_contato = ContatoModel(nome=nome, email=email, telefone=telefone, mensagem=mensagem)
+        db.session.add(novo_contato)
+        db.session.commit()
+        
+    
+        
+    return render_template('contatos.html', titulo = 'Contatos',formulario = formulario,dados_formulario = dados_formulario)
 
 #rota do sobre 
 
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html', titulo = 'Sobre')
-
-@app.route('/viagens')
-def viagens():
-    return render_template('viagens.html', titulo = 'Viagens')
